77880325e005ee5cec9596ba4fe126f37486f07d
Wei Zhou <leonzhouwei@gmail.com>
2017-03-02 11:38:54 +0800

    feat: FileSystemCat works

---
+++ src/main/java/com/example/learn/java/hdfs/Consts.java
@@ -0,0 +1,11 @@
+package com.example.learn.java.hdfs;
+
+public class Consts {
+	
+	public static final String USERNAME = "zhouwei";
+	public static final String URI_PREFIX = "hdfs://localhost:9000/user/" + USERNAME + "/";
+	
+	private Consts() {
+	}
+
+}
---
+++ src/main/java/com/example/learn/java/hdfs/FileSystemCat.java
@@ -0,0 +1,25 @@
+package com.example.learn.java.hdfs;
+
+import java.io.InputStream;
+import java.net.URI;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class FileSystemCat {
+	
+	private static final String FILE_NAME = "startup.sh";
+	private static final String FILE_URI = Consts.URI_PREFIX + FILE_NAME;
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(URI.create(FILE_URI), conf);
+		Path path = new Path(FILE_URI);
+		try (InputStream in = fs.open(path)) {
+			IOUtils.copy(in, System.out, 4096);
+		}
+	}
+
+}
