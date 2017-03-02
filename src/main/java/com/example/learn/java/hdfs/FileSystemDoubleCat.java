142c54c8cdfecc086b84d79f82d05a82396a7249
Wei Zhou <leonzhouwei@gmail.com>
2017-03-02 11:43:39 +0800

    feat: FileSystemDoubleCat works

---
+++ src/main/java/com/example/learn/java/hdfs/FileSystemDoubleCat.java
@@ -0,0 +1,27 @@
+package com.example.learn.java.hdfs;
+
+import java.net.URI;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class FileSystemDoubleCat {
+	
+	private static final String FILE_NAME = "startup.sh";
+	private static final String FILE_URI = Consts.URI_PREFIX + FILE_NAME;
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(URI.create(FILE_URI), conf);
+		Path path = new Path(FILE_URI);
+		try (FSDataInputStream in = fs.open(path)) {
+			IOUtils.copy(in, System.out, 4096);
+			in.seek(0);
+			IOUtils.copy(in, System.out, 4096);
+		}
+	}
+
+}
