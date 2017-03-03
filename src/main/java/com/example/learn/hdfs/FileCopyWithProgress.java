4669e269ffea9d2a2416c21a319dabbd918b7b79
Wei Zhou <leonzhouwei@gmail.com>
2017-03-03 17:08:11 +0800

    refactor: tiny renaming

--- src/main/java/com/example/learn/hdfs/FileCopyWithProgress.java
+++ src/main/java/com/example/learn/hdfs/FileCopyWithProgress.java
@@ -20,12 +20,11 @@ public class FileCopyWithProgress {
 
 	public static void main(String[] args) throws Exception {
 		String localSrc = Consts.POM_XML;
-		String dst = Consts.HDFS_URI_PREFIX + Consts.FILE_SEPARATOR + Consts.POM_XML;
 		InputStream in = new BufferedInputStream(new FileInputStream(localSrc));
 
 		Configuration conf = new Configuration();
-		FileSystem fs = FileSystem.get(URI.create(dst), conf);
-		OutputStream out = fs.create(new Path(dst), new Progressable() {
+		FileSystem fs = FileSystem.get(URI.create(Consts.POM_XML_HDFS_URI), conf);
+		OutputStream out = fs.create(new Path(Consts.POM_XML_HDFS_URI), new Progressable() {
 			@Override
 			public void progress() {
 				System.out.print(".");
--- src/main/java/com/example/learn/hdfs/consts/Consts.java
+++ src/main/java/com/example/learn/hdfs/consts/Consts.java
@@ -1,16 +1,18 @@
 package com.example.learn.hdfs.consts;
 
 public class Consts {
-	
+
 	public static final String FILE_SEPARATOR = "/";
-	
+
 	public static final String POM_XML = "pom.xml";
-	
+
 	public static final String NUM_ZERO = "0";
-	
+
 	public static final String USERNAME = "zhouwei";
 	public static final String HDFS_URI_PREFIX = "hdfs://localhost:9000/user/" + USERNAME;
-	
+
+	public static final String POM_XML_HDFS_URI = HDFS_URI_PREFIX + FILE_SEPARATOR + POM_XML;
+
 	private Consts() {
 	}
 
